0225eb3534e81b1fde1a2b875751e4dbe0299c6f
Daiki Umezawa <137897494+umejaki@users.noreply.github.com>
2023-08-19 08:34:12 +0000

    Initial Commit

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,4 @@
 class HomesController < ApplicationController
-  def about
+  def top
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#about"
+  root to: "homes#top"
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update]
   get "/homes/about" => "homes#about", as: "about"
