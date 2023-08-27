f1446a0cfc13fb4e19d53dc0fa61a343883226d1
Daiki Umezawa <137897494+umejaki@users.noreply.github.com>
2023-08-27 07:28:08 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   end
   
   def create
+    @user = current_user
     @book = Book.new(book_params)
     @book.user_id = current_user.id
   if @book.save
@@ -32,20 +33,28 @@ class BooksController < ApplicationController
   
   def edit
     @book = Book.find(params[:id])
+  if @book.user == current_user
+    render :edit
+  else
+    redirect_to books_path
+  end
   end
   
   
     def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
+    if @book.update(book_params)
     flash[:notice] = "You have updated book successfully."
-    redirect_to  book_path
+    redirect_to  book_path(@book.id)
+    else
+      render :edit
+    end
     end 
 
   
    def destroy
-    post_image = Book.find(params[:id])
-    post_image.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
    end
   
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   
   belongs_to :user
   
-  validates :title, presence: true 
+  validates :title, presence: true
   
   validates :body, presence: true
   validates :body, length: { maximum: 200 }
